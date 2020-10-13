a668cc7a99eb858029c01a2b3333976c2c13f293
Raj Kumar <techsupport@deepredink.com>
2020-10-13 10:11:33 +0530

    added new files

---
+++ sql-union-join.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	UNION vs. JOIN
+The join such as INNER JOIN or LEFT JOIN combines columns from two tables while the UNION combines rows from two queries.
+</body>
+</html>
---
+++ webhooks.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h5>Webhook</h5>
+	A webhook (also called a web callback or HTTP push API) is a way for an app to provide other applications with real-time information. A webhook delivers data to other applications as it happens, meaning you get data immediately.
+
+	<h5>Callback</h5>
+	A callback function is executed after the current effect is 100% finished.
+</body>
+</html>
