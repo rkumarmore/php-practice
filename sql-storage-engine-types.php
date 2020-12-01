c268e946188d57fe5a79de6614080676f624f8fb
Raj Kumar <rkumar.more@gmail.com>
2020-12-01 18:58:48 +0530

    sql questions were added

---
+++ sql-storage-engine-types.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title></title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	MySQL provides various storage engines for its tables as follows:
+	<ul>
+		<li>MyISAM</li>
+		<li>InnoDB</li>
+		<li>MERGE</li>
+		<li>MEMORY (HEAP)</li>
+		<li>ARCHIVE</li>
+		<li>CSV</li>
+		<li>FEDERATED</li>
+	</ul>
+</body>
+</html>
+
+
