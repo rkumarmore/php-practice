d1d4113f194c9ac741ef29126270bf4c7837fdf8
Raj Kumar <rkumar.more@gmail.com>
2021-02-04 17:00:07 +0530

    posty files were added

---
+++ imp-links.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Important Links</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<ul>
+		<li><a href="https://github.com/barryvdh/laravel-debugbar">laravel-debugbar</a></li>
+		<li><a href="https://github.com/codecourse/posty-traversy-media">laravel-Posty-tutorial-Code</a></li>
+		
+	</ul>
+</body>
